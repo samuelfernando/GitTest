09bb28672d07054713e6762005ba282587e48fe1
samf <samf@143.167.119.105>
2014-11-19 14:26:08 +0000

    First commit with Cool Function

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author samf
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GitTest gt = new GitTest();
+        int out = gt.coolFunction();
+        System.out.println("Wow cool function just gave me "+out);
+    }
+
+    private int coolFunction() {
+        return 42;
+    }
+    
+}
